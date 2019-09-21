a89174e1b3a89c63a1f62971e13642bab5eb4123
KorikaDEV <46706906+KorikaDEV@users.noreply.github.com>
2019-09-21 12:36:38 +0200

    fixed method for getting all dynamic bone children

--- Korikas Avatar Tools/Editor/EditorWindows/Bones/BoneHelper.cs
+++ Korikas Avatar Tools/Editor/EditorWindows/Bones/BoneHelper.cs
@@ -4,17 +4,33 @@ using System.Collections.Generic;
 public class BoneHelper : MonoBehaviour {
     
     public static DynamicBone[] getAllDynBones(Transform avatar){
+        Transform hips = avatar.gameObject.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Hips);
+        Transform[] hipschilds = checkForEveryChildren(hips, new List<Transform>()).ToArray();
+
         List<DynamicBone> result = new List<DynamicBone>();
 
-        int count = avatar.childCount;
-        for (int i = 0; i < count; i++)
+        foreach (Transform t in hipschilds)
         {
-            Transform t = avatar.GetChild(i);
             if(t.gameObject.GetComponent<DynamicBone>()){
                 result.Add(t.gameObject.GetComponent<DynamicBone>());
             }
         }
 
         return result.ToArray();
+    }
+
+    public static List<Transform> checkForEveryChildren(Transform t, List<Transform> tl){
+        int count = t.childCount;
+        if(count > 0){
+            for (int i = 0; i < count; i++)
+            {
+                Transform tf = t.GetChild(i);
+                tl.Add(tf);
+                tl = checkForEveryChildren(tf, tl);
+            }
+            return tl;
+        }else{
+            return tl;
+        }
     }
 }
